d36af71106a8163282b738af0ddbc3531156a6a0
kisscool62 <kisscool62@gmail.com>
2014-12-21 14:26:18 +0100

    remove all unused files

--- getting_data/concatenate_2files.R
+++
@@ -1,9 +0,0 @@
-
-# file1 is the file to be concatenated to
-# file2 is the file to concatenate
-# result is a file = file1 + file2
-concatenate_2files <- function(file1, file2){
-        dataset1 <- readLines(f1<-file(file1));close(f1)
-        dataset2 <- readLines(f2<-file(file2));close(f2)
-        c(dataset1, dataset2)  
-}
