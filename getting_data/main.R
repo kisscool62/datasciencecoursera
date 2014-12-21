4438f9a84a1d7682e0f1ddbd0161f2d1c8b142fa
kisscool62 <kisscool62@gmail.com>
2014-12-21 14:20:17 +0100

    main to run run_analysis.R; result tidy_data_set.csv

---
+++ getting_data/main.R
@@ -0,0 +1,9 @@
+## just to run run_analysis.R
+
+
+working_directory <- '/Users/pascalauregan/workspace/r/coursera/03_getting_data/datasciencecoursera/getting_data'
+setwd(working_directory)
+
+source('run_analysis.R')
+
+
